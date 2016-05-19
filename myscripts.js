7eca3a7a56fa64f6f5f020a3b16ba85c7e48b218
shaharsprontz <shahar@getstocks.com>
2016-05-19 18:28:15 +0300

    Add files via upload

--- myscripts.js
+++ myscripts.js
@@ -3,8 +3,8 @@
  */
 
 
-if (screen.width <= 375) {
-    document.getElementById("vertical").classList.remove("verticalLine");
+if (screen.width <= 360) {
+    document.getElementById("vertical", "leftCullom").classList.remove("verticalLine", "professionalExperience");
 }else {
     document.getElementsByClassName("verticalLine").className = "verticalLine";
 }
