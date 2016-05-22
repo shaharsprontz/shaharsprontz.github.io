7df4284464b58c2801cf2a3320db00e4e494dd65
shaharsprontz <shahar@getstocks.com>
2016-05-22 22:20:02 +0300

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,10 @@
+/**
+ * Created by shahar on 19/05/16.
+ */
+
+
+if (screen.width <= 740) {
+    document.getElementById("leftContain").classList.remove("leftContainer");
+}else {
+    document.getElementById("leftContain").className = "leftContainer";
+}
